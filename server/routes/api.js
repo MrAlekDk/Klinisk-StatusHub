e79e7141af021982f8c3f1e959c2a3ac7851d86a
Calok <72436270+CalokTI@users.noreply.github.com>
2026-05-01 13:49:04 +0200

    Added dotenv, test endpoints for api and db
    Added .env dependency
    Added test endpoints for api and db connection, which only works while in development

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,4 +6,22 @@ const router = express.Router();
 //Routes for API endpoints here
 
 
+//test route to check if API is working
+if (process.env.NODE_ENV == 'development') {
+    router.get('/api-test', (req, res) => {
+        res.json({ message: 'API is working' });
+    });
+
+
+    router.get('/db-test', (req, res) => {
+        try {
+            const stmt = db.prepare('SELECT 1');
+            const result = stmt.get();
+            res.json({ ok: true, result });
+        } catch (err) {
+            res.status(500).json({ ok: false, error: err.message });
+        }
+    });
+}
+
 module.exports = router;
