2660c5bdfd26bcc36eff520013e3a25b38d95c32
Alexander Sørensen <alexandersoerensen.dev@gmail.com>
2026-05-01 14:10:00 +0200

    Connected inputs in login-component
    Added NgForms module for 2way binding of the inputs in the login component
    Added some styling for the components + background

--- client/src/app/components/login-component/login-component.component.ts
+++ client/src/app/components/login-component/login-component.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-component.component.html',
   styleUrl: './login-component.component.css'
 })
 export class LoginComponentComponent {
+  email: string = "";
+  password: string = "";
+
+  onSubmit(email:string, password:string) {
+    console.log(email, password);
+  }
 
 }
