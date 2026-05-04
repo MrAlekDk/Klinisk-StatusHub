e8b0313a035af0b3253d8f08771d85fc92079e76
Calok <72436270+CalokTI@users.noreply.github.com>
2026-05-04 11:23:27 +0200

    Schema and database
    Added schema for database generation, separated into it's own file, schema.sql.

--- server/db/database.js
+++ server/db/database.js
@@ -1,9 +1,15 @@
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 
 const dbPath = path.join(__dirname, 'database.sqlite');
+const schemaPath = path.join(__dirname, 'schema.sql');
 const db = new Database(dbPath);
 
+db.exec(`PRAGMA foreign_keys = ON;`);
 
+const schema = fs.readFileSync(schemaPath, 'utf-8');
+
+db.exec(schema);
 
 module.exports = db;
